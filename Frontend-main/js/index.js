6247e228c69514dce39441ebdf46ead73443cbbb
Quszlet <103947062+Quszlet@users.noreply.github.com>
2023-01-12 17:03:26 +0300

    Delete Frontend-main directory

--- Frontend-main/js/index.js
+++
@@ -1,2 +0,0 @@
-makeRelocateButton('sign-in-btn', 'html/sign-in.html')
-makeRelocateButton('sign-up-btn', 'html/sign-up.html')
--- Frontend-main/js/redict.js
+++
@@ -1,9 +0,0 @@
-function makeRelocateButton(buttonId, pageUrl) {
-    document.getElementById(buttonId).addEventListener('click', () => {
-        document.location.href = pageUrl
-    })
-}
-
-function pageRedict(pageUrl) {
-        document.location.href = '../html/' + pageUrl + ".html"
-}
